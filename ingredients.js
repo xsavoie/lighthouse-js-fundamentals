438404973a2ca43080761b7dce1a9242479af5a3
Xavier Savoie <xaviersavoie@gmail.com>
2021-07-23 13:30:07 +0000

    Iterating over arrays

--- ingredients.js
+++ ingredients.js
@@ -1,7 +1,6 @@
 const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
 
 // Write a while loop that prints out the contents of ingredients:
-/*
 let i = 0;
 while (i < ingredients.length) {
   console.log(ingredients[i])
@@ -11,7 +10,7 @@ while (i < ingredients.length) {
 // Write a for loop that prints out the contents of ingredients:
 for (i = 0; i < ingredients.length; i++) {
   console.log(ingredients[i])
-}*/
+}
 
 // Write any loop (while or for) that prints out the contents of ingredients backwards:
 for (i = ingredients.length - 1; i >= 0; i--) {
